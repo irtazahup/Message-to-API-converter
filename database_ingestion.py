1abd15e909a3c611c59d0f341f86c8cc1560ff35
IrtezaIshaq <150837618+irtazahup@users.noreply.github.com>
2026-03-02 15:23:26 +0500

    insertion in db done - update database_ingestion.py

--- database_ingestion.py
+++ database_ingestion.py
@@ -2,12 +2,24 @@ from supabase import create_client, Client
 import dotenv
 import os
 from brain import extract_carpool_info
-
+import json
 
 SUPABASE_URL=os.getenv("SUPABASE_URL")
 SUPABASE_ANON_KEY=os.getenv("SUPABASE_ANON_KEY")
 
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_ANON_KEY)
 
-sample=extract_carpool_info("Heading to the mall at 5pm, 2 seats free")
+sample_raw=extract_carpool_info("Heading to the mall at 5pm, 2 seats free")
+
+sample = json.loads(sample_raw)
+
+# 3. Now you can add the manual field
+sample['user_name'] = 'Whatsapp User'
+
 print(sample)
+
+try:
+    response = supabase.table("carpool_entries").insert(sample).execute()
+    print("Data inserted successfully:", response.data)
+except Exception as e:
+    print("Error inserting data:", e)
